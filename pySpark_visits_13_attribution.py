01a32780582bbe2961e0ed358584b79c4603bd02
Ёжик <43043736+ejichek@users.noreply.github.com>
2024-12-02 13:41:52 +0300

    Update pySpark_visits_13_attribution.py

--- pySpark_visits_13_attribution.py
+++ pySpark_visits_13_attribution.py
@@ -44,7 +44,7 @@ vsiits_13_attribution_schema = StructType([
 ])
 
 df1 = spark.read \
-    .option("delimiter", "|") \
+    .option("delimiter", "\t") \
     .schema(vsiits_13_attribution_schema) \
     .csv("/user/azhalybin/airflow/test/txt/final_txt.txt")
 
